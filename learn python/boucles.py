5bfcdf78f07c8209baecb5c95706cf8db20c022f
Raphaël <raphael.namyst@gmail.com>
2026-03-27 12:36:01 +0000

    car il faut être sympa

--- learn python/boucles.py
+++ learn python/boucles.py
@@ -1,4 +1,20 @@
 #ce fichier vise à apprendre les boucles for et while
 
 def affiche(tab: list) -> list:
-    pass
+    L = []
+    for i in range():
+        #L doit finir avex chaque élément de tab
+        pass
+
+
+def convert_in_binaire(n: int) -> int:
+    L = []
+    while():
+        #prend le reste de la division par 2 et l'ajoute à L
+        pass
+    while():
+        #retourner la list
+        pass
+
+#tests
+# à écrire        
